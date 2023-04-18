5e0fb6486ea890e4445317d508d8e432fb0850eb
kirk admin <kirkalainandes@gmail.com>
2023-04-18 11:41:15 +0800

    add FPS counter

--- index.js
+++ index.js
@@ -98,3 +98,19 @@ function closeFullscreen() {
     document.mozCancelFullScreen();
   }
 }
+
+var fps = document.getElementById("fps");
+var startTime = Date.now();
+var frame = 0;
+
+function tick() {
+  var time = Date.now();
+  frame++;
+  if (time - startTime > 1000) {
+    fps.innerHTML = (frame / ((time - startTime) / 1000)).toFixed(1);
+    startTime = time;
+    frame = 0;
+  }
+  window.requestAnimationFrame(tick);
+}
+tick();
